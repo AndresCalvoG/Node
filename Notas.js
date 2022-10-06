071407a58923e13d23dfee6b2adf5bdf0ef3e74c
AndresCalvoG <andrescalvo9407@gmail.com>
2022-10-05 20:13:23 -0500

    end class 2 origines

---
+++ Notas.js
@@ -0,0 +1,16 @@
+/*
+1) realizamos la intalacion desde la web de Node.js
+  ejecutamos el comando node -v en la terminal para saber la version 
+  que estamos trabajando.
+
+  Que es NodeJS?
+  es un entorno de ejecucion de javascript fuera del navegador y se creo
+  en 2009 orientado a servidores
+
+  * es concurrente, osea que es monohilo, con entradas y salidas asincronas
+  un proceso por cada nucleo del procesador
+  * funciona con el motor V8 de google creado en 2008 escrito en c++,
+  convierte js en codigo maquina en lugar de interpretarlo en tiempo real
+  *existe un bucle de eventos que se ejecuta constantemente asi que puedes 
+  orientar el codigo de forma reactiva.
+*/
