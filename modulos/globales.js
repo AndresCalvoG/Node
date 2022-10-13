806f5b2c0832b9cdc28aea253666b2e04aca88f6
AndresCalvoG <andrescalvo9407@gmail.com>
2022-10-12 20:06:22 -0500

    end class 11 globales

---
+++ modulos/globales.js
@@ -0,0 +1,8 @@
+//console.log(global);
+//setInterval()
+//clearInterval()
+//setImmediate()
+//console.log(process)
+//console.log(__dirname);
+//console.log(__filename);
+//global.miVar = 'elvalor'
